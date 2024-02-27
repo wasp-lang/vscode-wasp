a034c5e7cc6e2bd299db65e8a1824b8a8ecef6d1
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-02-27 19:40:51 +0100

    Made extension work for both Wasp 0.12 and for older versions and also formatted the code.
    Co-authored-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,31 @@
-import * as os from 'os';
-import { workspace, ExtensionContext, window, commands } from 'vscode';
-
+import * as os from "os";
+import {
+  languages,
+  workspace,
+  ExtensionContext,
+  window,
+  commands,
+  CompletionItem,
+  SnippetString,
+} from "vscode";
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-  TransportKind
-} from 'vscode-languageclient/node';
-import { promisify } from 'util';
-import { execFile } from 'child_process';
+  TransportKind,
+} from "vscode-languageclient/node";
+import { promisify } from "util";
+import { execFile } from "child_process";
 
 let client: LanguageClient | null = null;
 
-const outputChannel = window.createOutputChannel('Wasp Language Server');
+const outputChannel = window.createOutputChannel("Wasp Language Server");
 
 export async function activate(context: ExtensionContext): Promise<void> {
   console.log('..Extension "vscode-wasp" is now active..');
 
   // Configuration name and properties are set in package.json
-  const config = workspace.getConfiguration('wasp');
+  const config = workspace.getConfiguration("wasp");
 
   const executablePath = resolveWaspExecutablePath(config.server.executable);
 
@@ -27,12 +34,14 @@ export async function activate(context: ExtensionContext): Promise<void> {
 
   if (typeof waspExecResult !== "string") {
     if (waspExecResult.type === "WaspVersionTimedout") {
-      window.showErrorMessage('The wasp server process has timed out.');
+      window.showErrorMessage("The wasp server process has timed out.");
     } else if (waspExecResult.type === "WaspExeMissing") {
-      if (executablePath === 'wasp') {
-        window.showErrorMessage('No `wasp` executable is available in the VSCode PATH.');
+      if (executablePath === "wasp") {
+        window.showErrorMessage("No `wasp` executable is available in the VSCode PATH.");
       } else {
-        window.showErrorMessage(`The user defined executable path couldn't be run: ${executablePath} . More details: ${waspExecResult.error}`);
+        window.showErrorMessage(
+          `The user defined executable path couldn't be run: ${executablePath} . More details: ${waspExecResult.error}`,
+        );
       }
     }
     return;
@@ -41,24 +50,39 @@ export async function activate(context: ExtensionContext): Promise<void> {
   const waspVersion = waspExecResult;
   outputChannel.appendLine("Confirmed that wasp executable is accessible, version " + waspVersion);
 
-  const minSupportedWaspVersion = "0.12.0"
+  const minSupportedWaspVersion = "0.6.0";
   if (compareSimpleSemvers(waspVersion, minSupportedWaspVersion) === -1) {
     window.showErrorMessage(
-      `Version of your installed wasp executable is ${waspVersion}, but this VSCode extension supports only`
-      + ` wasp >= ${minSupportedWaspVersion}. Either update wasp or downgrade this extension.`
+      `Version of your installed wasp executable is ${waspVersion}, but this VSCode extension supports only` +
+        ` wasp >= ${minSupportedWaspVersion}. Either update wasp or downgrade this extension.`,
     );
     return;
   }
+
+  // Register snippets for .wasp files.
+  const snippets = getSnippets(waspVersion);
+  const provider = languages.registerCompletionItemProvider("wasp", {
+    provideCompletionItems(document, position) {
+      // Return the appropriate snippets as completion items.
+      return snippets.map((snippet) => {
+        const completionItem = new CompletionItem(snippet.prefix);
+        completionItem.insertText = new SnippetString(snippet.body.join("\n"));
+        completionItem.detail = snippet.description;
+        return completionItem;
+      });
+    },
+  });
+  context.subscriptions.push(provider);
 
   // TODO: send these settings to wasp language server so it can update its logging output if
   // these are changed while the language server is running
   const useOutputPanel = config.server.useOutputPanelForLogging;
   const logFile = useOutputPanel ? "[OUTPUT]" : config.server.logFile;
-  const logFileOpt = logFile.trim() === '' ? [] : ['--log=' + logFile];
+  const logFileOpt = logFile.trim() === "" ? [] : ["--log=" + logFile];
 
   // Configure vscode-languageclient
-  const runArgs = ['waspls', ...logFileOpt];
-  const debugArgs = ['waspls', ...logFileOpt];
+  const runArgs = ["waspls", ...logFileOpt];
+  const debugArgs = ["waspls", ...logFileOpt];
 
   const serverOptions: ServerOptions = {
     run: {
@@ -70,53 +94,54 @@ export async function activate(context: ExtensionContext): Promise<void> {
       command: executablePath,
       transport: TransportKind.stdio,
       args: debugArgs,
-    }
+    },
   };
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: 'file', language: 'wasp' }],
-    initializationOptions: { 'vscode-wasp': workspace.getConfiguration('vscode-wasp') },
-    outputChannel: outputChannel
+    documentSelector: [{ scheme: "file", language: "wasp" }],
+    initializationOptions: {
+      "vscode-wasp": workspace.getConfiguration("vscode-wasp"),
+    },
+    outputChannel: outputChannel,
   };
 
-  client = new LanguageClient(
-    'vscode-wasp',
-    'VSCode Wasp',
-    serverOptions,
-    clientOptions,
-  );
+  client = new LanguageClient("vscode-wasp", "VSCode Wasp", serverOptions, clientOptions);
 
-  outputChannel.appendLine('Starting Wasp LSP Server..');
-  client.start()
+  outputChannel.appendLine("Starting Wasp LSP Server..");
+  client
+    .start()
     .then(() => {
-      outputChannel.appendLine('..Wasp LSP Server has been successfully started!');
+      outputChannel.appendLine("..Wasp LSP Server has been successfully started!");
     })
     .catch(() => {
-      let failedToConnectErrorMsg = "Failed to connect to Wasp language server! Advanced Wasp IDE features won't work."
-      const earliestWaspVersionWithLspCapabilities = "0.6.0.0"
+      let failedToConnectErrorMsg =
+        "Failed to connect to Wasp language server! Advanced Wasp IDE features won't work.";
+      const earliestWaspVersionWithLspCapabilities = "0.6.0.0";
       if (compareSimpleSemvers(waspVersion, earliestWaspVersionWithLspCapabilities) === -1) {
-        failedToConnectErrorMsg += ` CAUSE: Your Wasp version is ${waspVersion}, but it should be at least ${earliestWaspVersionWithLspCapabilities}.`
+        failedToConnectErrorMsg += ` CAUSE: Your Wasp version is ${waspVersion}, but it should be at least ${earliestWaspVersionWithLspCapabilities}.`;
       }
       outputChannel.appendLine(".." + failedToConnectErrorMsg);
       // NOTE: I use timeout here so this error message doesn't get printed at the same time as the error messages produced by the
       // LanguageClient itself (and it produces a bunch of them), because if they all get very close in time vscode hides them immediately
       // due to there being too many of them at the same moment. By using timeout, our message gets separated and stays for some time as a popup,
       // instead of skipping directly into notification tray.
-      setTimeout(() => window.showErrorMessage(failedToConnectErrorMsg), 1000)
+      setTimeout(() => window.showErrorMessage(failedToConnectErrorMsg), 1000);
     });
 
   // Register command to restart wasp language server.
-  context.subscriptions.push(commands.registerCommand('vscode-wasp.restartLanguageServer', async () => {
-    if (!client) return;
-    try {
-      outputChannel.appendLine(`Restarting Wasp LSP Server..`);
-      await client.restart();
-      outputChannel.appendLine(`..Wasp LSP Server has been restarted!`);
-    } catch (err) {
-      const errMsg = err instanceof Error ? err.message : err
-      outputChannel.appendLine(`Couldn't restart Wasp LSP Server: ${errMsg}`);
-    }
-  }));
+  context.subscriptions.push(
+    commands.registerCommand("vscode-wasp.restartLanguageServer", async () => {
+      if (!client) return;
+      try {
+        outputChannel.appendLine(`Restarting Wasp LSP Server..`);
+        await client.restart();
+        outputChannel.appendLine(`..Wasp LSP Server has been restarted!`);
+      } catch (err) {
+        const errMsg = err instanceof Error ? err.message : err;
+        outputChannel.appendLine(`Couldn't restart Wasp LSP Server: ${errMsg}`);
+      }
+    }),
+  );
 }
 
 export function deactivate(): void {
@@ -129,9 +154,9 @@ export function deactivate(): void {
 // returns interpolated executable path that can then be normally used.
 // If no path is given, it returns default path to wasp executable.
 function resolveWaspExecutablePath(userDefinedWaspExecutablePath: string): string {
-  if (!userDefinedWaspExecutablePath) return 'wasp';
+  if (!userDefinedWaspExecutablePath) return "wasp";
   console.log(`Trying to find the server executable in ${userDefinedWaspExecutablePath}`);
-  const resolvedPath = interpolateVSCodeSpecificExecutablePath(userDefinedWaspExecutablePath)
+  const resolvedPath = interpolateVSCodeSpecificExecutablePath(userDefinedWaspExecutablePath);
   console.log(`Location after path variables subsitution: ${resolvedPath}`);
   return resolvedPath;
 }
@@ -143,8 +168,8 @@ function interpolateVSCodeSpecificExecutablePath(executablePath: string): string
 
   // Path variable for home directory
   executablePath = executablePath
-    .replace('${HOME}', os.homedir)
-    .replace('${home}', os.homedir)
+    .replace("${HOME}", os.homedir)
+    .replace("${home}", os.homedir)
     .replace(/^~/, os.homedir);
 
   // Path variable for workspace folder
@@ -153,12 +178,12 @@ function interpolateVSCodeSpecificExecutablePath(executablePath: string): string
     const folder = folders[0];
     if (folder) {
       executablePath = executablePath
-        .replace('${workspaceFolder}', folder.uri.path)
-        .replace('${workspaceRoot}', folder.uri.path);
+        .replace("${workspaceFolder}", folder.uri.path)
+        .replace("${workspaceRoot}", folder.uri.path);
     }
   }
 
-  return executablePath
+  return executablePath;
 }
 
 // Runs the wasp executable in `version` mode and checks that it:
@@ -167,25 +192,39 @@ function interpolateVSCodeSpecificExecutablePath(executablePath: string): string
 //
 // If it fails either of these checks, a status is returned to show that the
 // executable path is not valid.
-async function getWaspExecutableVersion(executablePath: string): Promise<WaspVersionError | string> {
+async function getWaspExecutableVersion(
+  executablePath: string,
+): Promise<WaspVersionError | string> {
   const execFileP = promisify(execFile);
-  const execPromise = execFileP(executablePath, ['version'], { timeout: 2000, windowsHide: true })
-    .then(({ stdout }) => stdout.split("\n")[0])  // We assume `wasp version` returns version as a single string in the first line, with nothing else.
-    .catch((e) => ({ type: "WaspExeMissing", error: e } as WaspExeMissing));
+  const execPromise = execFileP(executablePath, ["version"], {
+    timeout: 2000,
+    windowsHide: true,
+  })
+    .then(({ stdout }) => stdout.split("\n")[0]) // We assume `wasp version` returns version as a single string in the first line, with nothing else.
+    .catch((e) => ({ type: "WaspExeMissing", error: e }) as WaspExeMissing);
 
-  const timeoutPromise = new Promise<WaspVersionTimedout>((resolve) => setTimeout(() => resolve({ type: "WaspVersionTimedout" } as WaspVersionTimedout), 1000));
+  const timeoutPromise = new Promise<WaspVersionTimedout>((resolve) =>
+    setTimeout(() => resolve({ type: "WaspVersionTimedout" } as WaspVersionTimedout), 1000),
+  );
 
   return Promise.race([execPromise, timeoutPromise]);
 }
 
-type WaspVersionError = WaspExeMissing | WaspVersionTimedout
-interface WaspExeMissing { type: "WaspExeMissing", error: any }
-interface WaspVersionTimedout { type: "WaspVersionTimedout" }
+type WaspVersionError = WaspExeMissing | WaspVersionTimedout;
+interface WaspExeMissing {
+  type: "WaspExeMissing";
+  error: any;
+}
+interface WaspVersionTimedout {
+  type: "WaspVersionTimedout";
+}
 
 // Given two semantic version strings, returns -1 if first version is smaller than the second,
 // 0 if they are the same, or 1 if first version is bigger than the second.
 function compareSimpleSemvers(v1: string, v2: string): -1 | 0 | 1 {
-  const LT = -1, GT = 1, EQ = 0;
+  const LT = -1,
+    GT = 1,
+    EQ = 0;
   const v1Parsed = parseSemver(v1);
   const v2Parsed = parseSemver(v2);
   for (let i = 0; i < Math.min(v1Parsed.length, v2Parsed.length); i++) {
@@ -199,5 +238,129 @@ function compareSimpleSemvers(v1: string, v2: string): -1 | 0 | 1 {
 
 // Parses simple semver string into list of numbers.
 function parseSemver(v: string): number[] {
-  return v.split('.').map(n => parseInt(n))
+  return v.split(".").map((n) => parseInt(n));
+}
+
+type Snippet = {
+  prefix: string;
+  description: string;
+  body: string[];
 };
+
+function getSnippets(waspVersion: string): Snippet[] {
+  const [major, minor] = parseSemver(waspVersion);
+
+  const [extImportClientPrefix, extImportServerPrefix] =
+    major == 0 && minor <= 11 ? ["@client", "@server"] : ["@src", "@src"];
+
+  const snippetPageAndRouteBody = [
+    'route ${1:Name}Route { path: "/${2:path}", to: ${1:Name} }',
+    "page ${1:Name} {",
+    `\tcomponent: import \${1:name} from "${extImportClientPrefix}/\${3:sourceFile}"`,
+    "}",
+  ];
+
+  const snippets = [
+    {
+      prefix: "page",
+      description: "[snippet] Create a new page and route",
+      body: snippetPageAndRouteBody,
+    },
+
+    {
+      prefix: "route",
+      description: "[snippet] Create a new page and route",
+      body: snippetPageAndRouteBody,
+    },
+
+    {
+      prefix: "route",
+      description: "[snippet] Create a new route",
+      body: ['route ${1:Name}Route { path: "/${2:path}", to: ${3:PageName} }'],
+    },
+
+    {
+      prefix: "query",
+      description: "[snippet] Create a new query",
+      body: [
+        "query ${1:name} {",
+        `\tfn: import \${2:function} from "${extImportServerPrefix}/\${3:sourceFile}",`,
+        "\tentities: [$4]",
+        "}",
+      ],
+    },
+
+    {
+      prefix: "action",
+      description: "[snippet] Create a new action",
+      body: [
+        "action ${1:name} {",
+        `\tfn: import \${2:function} from "${extImportServerPrefix}/\${3:sourceFile}",`,
+        "\tentities: [$4]",
+        "}",
+      ],
+    },
+
+    {
+      prefix: "entity",
+      description: "[snippet] Create a new entity",
+      body: ["entity ${1:Name} {=psl", "\t${2:schema}", "psl=}"],
+    },
+
+    {
+      prefix: "job",
+      description: "[snippet] Create a new job",
+      body: [
+        "job ${1:Name} {",
+        "\texecutor: PgBoss,",
+        "\tperform: {",
+        `\t\tfn: import \${2:FnName} from "${extImportServerPrefix}/\${3:sourceFile}"`,
+        "\t},",
+        "\tentities: [$4],",
+        "}",
+      ],
+    },
+
+    {
+      prefix: "api",
+      description: "[snippet] Create a new api route",
+      body: [
+        "api ${1:name} {",
+        '\thttpRoute: (${2:GET}, "${3:path}"),',
+        `\tfn: import \${4:function} from "${extImportServerPrefix}/\${5:sourceFile}",`,
+        "\tentities: [$6]",
+        "}",
+      ],
+    },
+
+    {
+      prefix: "apiNamespace",
+      description: "[snippet] Create a new api namespace",
+      body: [
+        "apiNamespace ${1:name} {",
+        `\tmiddlewareConfigFn: import \${2:middlewareFn} from "${extImportServerPrefix}/\${3:sourceFile}",`,
+        '\tpath: "${4:path}"',
+        "}",
+      ],
+    },
+
+    {
+      prefix: "crud",
+      description: "[snippet] Create a new crud",
+      body: [
+        "crud ${1:Name} {",
+        "\tentity: ${2:EntityName},",
+        "\toperations: {",
+        "\t\tget: {},",
+        "\t\tgetAll: {},",
+        "\t\tcreate: {},",
+        "\t\tupdate: {},",
+        "\t\tdelete: {},",
+        "\t},",
+        "}",
+      ],
+    },
+  ];
+
+  return snippets;
+}
